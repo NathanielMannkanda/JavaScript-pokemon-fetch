11d450779b143b52ad29c5b216b17f8dc875343b
RakeemOsama <heavenlyecstacy06@gmail.com>
2025-08-14 13:02:56 +0200

    used async/await fto fetch the pokemon

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
   .then(response => {
     //for error
@@ -8,3 +9,23 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
   })
   .then(data => console.log(data.id))
   .catch(error => console.log(error))
+*/
+
+//async/await version
+fetchData();
+async function fetchData() {
+  
+  try{
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
+
+    if (!response.ok){
+      throw new Error("could not fetch resource");
+    }
+
+    const data = await response.json()
+    console.log(data)
+  }
+  catch(error){
+    console.error(error)
+  }
+}
